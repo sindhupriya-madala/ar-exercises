49e6c72ed8245c551180fca958ccc083d178888d
sindhupriya-madala <madala.sindhu@gmail.com>
2017-05-09 23:16:55 +0000

    worked with .sum and .count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+sum = Store.all.sum(:annual_revenue)
+p "sum of annual_revenue of all stores is: #{sum}"
+count = Store.count
+
+p "average annual_revenue of all stores is: #{sum/count}"
+
+p Store.where("annual_revenue > ?", 1000000).count
 # Your code goes here ...
