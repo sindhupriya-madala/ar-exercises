da20910396f55197c44ebe5ca033923acf6ecf3a
sindhupriya-madala <madala.sindhu@gmail.com>
2017-05-09 21:27:11 +0000

    update active record using instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: 'vancouver')
+
 # Your code goes here ...
