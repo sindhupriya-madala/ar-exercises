0857e311d9d06366d40a09997cac5307aafda353
sindhupriya-madala <madala.sindhu@gmail.com>
2017-05-09 21:20:04 +0000

    exercise1 insert data using class.create.save

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,9 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel:true).save
+Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel:true).save
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true).save
+
+puts Store.all.count
 # Your code goes below here ...
